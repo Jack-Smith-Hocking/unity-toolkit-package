ba79fc6dd0e854d9938f30c55c4bf0047ff99afc
Jack Smith-Hocking <hockingjack818@gmail.com>
2026-03-15 17:37:49 +1030

    Added IntAsset and SerialisedDictionary

---
+++ Runtime/IntAsset.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace LordSheo
+{
+	[CreateAssetMenu(menuName = "LordSheo/IntAsset", fileName = "IntAsset")]
+	public class IntAsset : ScriptableObject
+	{
+		public int value;
+	}
+}
