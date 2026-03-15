e74ad2c51e84a4f59eaf9dd569d350bc7d575b1f
Jack Smith-Hocking <hockingjack818@gmail.com>
2026-03-15 17:37:40 +1030

    Added RandUtil

---
+++ Runtime/Util/RandUtil.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace LordSheo
+{
+	public static class RandUtil
+	{
+		public static Vector3 Random(Vector3 min, Vector3 max)
+		{
+			var result = new Vector3();
+
+			result.x = UnityEngine.Random.Range(min.x, max.x);
+			result.y = UnityEngine.Random.Range(min.y, max.y);
+			result.z = UnityEngine.Random.Range(min.z, max.z);
+            
+			return result;
+		}
+	}
+}
